e9fe4d39168d985748fdd6c5190d7a4a2e42d9e9
Cristina Velasco Rúa <99815693+critinaVelasco@users.noreply.github.com>
2026-06-13 21:16:22 +0200

    Add files via upload

---
+++ src/components/VerPorras.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function VerPorras() {
+    return (
+        <div>
+            <p>Ventana en desarrollo, dadme tiempo.</p>
+        </div>
+    );
+}
+
+export default Reglas;
